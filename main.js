bc251baf4e4a465cd3c4faa427d5972bc3d7b2de
mgarcia7335 <joooo.2008@yahoo.com.ar>
2024-08-31 19:57:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// seleccionar los botones de las tarjetas
+
+
+document.querySelector('.boton1').addEventListener('click', function () {
+    alert('Has click en la tarjeta 1')
+});
+
+document.querySelector('.boton2').addEventListener('click', function () {
+    alert('Has click en la tarjeta 2')
+});
+
+document.querySelector('.boton3').addEventListener('click', function () {
+    alert('Has click en la tarjeta 3')
+});
+
+
+    
